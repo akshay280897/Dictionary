4e87843388aa247b5ce39bb81fb2a7d588f05215
akshay280897 <akipotdar@gmail.com>
2017-12-23 22:28:51 +0530

    optimizing output

--- MyApp.py
+++ MyApp.py
@@ -7,9 +7,21 @@ def translate(word):
     if word in data:
         return(data[word])
     elif len(get_close_matches(word,data.keys())) > 0:
-        return "\nDid you mean %s " % get_close_matches(word,data.keys())[0]
+        yn = input("\nDid you mean %s instead ? if yes 'Y' or no 'N' : " % get_close_matches(word,data.keys())[0])
+        if yn == 'Y':
+            return data[get_close_matches(word,data.keys())[0]]
+        elif yn == 'N':
+            return "Word Doesn't exists"
+        else:
+            return "Wrong input"
     else:
         return "Word doesn't exist"
 
 inputWord = input("enter the word : ")
-print(translate(inputWord))
+output = translate(inputWord)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
