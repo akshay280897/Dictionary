fca5a34fc69318ce9f1da08c23d086a0714ed9cd
akshay280897 <akipotdar@gmail.com>
2017-12-23 21:28:02 +0530

    making word case insensitive and find close matching pattern

--- MyApp.py
+++ MyApp.py
@@ -1,9 +1,13 @@
 import json
+from difflib import  get_close_matches
 data = json.load(open("data.json"))
 
 def translate(word):
-    if inputWord in data:
+    word = word.lower()     #making word case insensitive
+    if word in data:
         return(data[word])
+    elif len(get_close_matches(word,data.keys())) > 0:
+        return "\nDid you mean %s " % get_close_matches(word,data.keys())[0]
     else:
         return "Word doesn't exist"
 
