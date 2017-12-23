bfcab817b87a37bbf4878f193663e51165c40612
akshay280897 <akipotdar@gmail.com>
2017-12-23 20:52:59 +0530

    returning definition of the word

--- MyApp.py
+++ MyApp.py
@@ -1,3 +1,11 @@
 import json
 data = json.load(open("data.json"))
-print(data)
+
+def translate(word):
+    if inputWord in data:
+        return(data[word])
+    else:
+        return "Word doesn't exist"
+
+inputWord = input("enter the word : ")
+print(translate(inputWord))
